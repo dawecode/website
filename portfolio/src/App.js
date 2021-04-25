3d378e8754d1d431190f7b55384366936818d24d
Catherine Dawe <dawe.cm@gmail.com>
2021-04-25 19:30:19 -0400

    add stucture for the Navbar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,32 +1,14 @@
-import logo from "./logo.svg";
-import { BrowserRouter, Route, Link } from "react-router-dom";
-import Projects from "./Projects.js";
-import Articles from "./Articles.js";
-import About from "./About.js";
-import "./App.css";
+import React from 'react'
+import { Header} from './components/common'
+
 
 function App() {
   return (
-    <BrowserRouter>
-     {/* set up Router */}
-      <div className="App">
-        <Route exact path="/" component={Projects} />
-        <Route path="/articles" component={Articles} />
-        <Route path="/about" component={About} />
-
-        <div className="navigation">
-          {/* <img src={logo} className="logo" alt="Logo" /> */}
-          <div className="navigation-sub">
-
-            {/* set up the Links  */}
-            <Link to="/" className="item">Projects</Link>
-            <Link to="/articles" className="item">Articles</Link>
-            <Link to="/about" className="item">About</Link>
-     
-          </div>
-        </div>
-      </div>
-    </BrowserRouter>
+   <div className="App">
+     <Header />
+  
+   </div>
+    
   );
 }
 
