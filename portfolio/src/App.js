62a7e6fa6afffe0f33805d9a34e6c76f90e2f17c
Catherine Dawe <dawe.cm@gmail.com>
2021-04-25 16:23:52 -0400

    set up router and browser router

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import Projects from "./Projects.js";
+import Articles from "./Articles.js";
+import About from "./About.js";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+     {/* set up Router */}
+      <div className="App">
+        <Route exact path="/" component={Projects} />
+        <Route path="/articles" component={Articles} />
+        <Route path="/about" component={About} />
+
+        <div className="navigation">
+          <img src={logo} className="logo" alt="Logo" />
+          <div className="navigation-sub">
+
+            {/* set up the Links  */}
+            <Link to="/" className="item">Projects</Link>
+            <Link to="/articles" className="item">Articles</Link>
+            <Link to="/about" className="item">About</Link>
+     
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
